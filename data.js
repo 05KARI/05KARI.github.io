255637bf325fe3fd0b9be76fa15693edaa61d03c
05KARI <106485649+05KARI@users.noreply.github.com>
2023-07-26 23:22:06 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,17 @@
 json=[
 {"text":"Ivy Injector","dest":"https://05kari.github.io/Ivy","new":true}
 ]
+
+function Get(obj){return document.querySelector(obj)}
+
+json.forEach(data=>{
+a=document.createElement("a")
+a.innerHTML=data.text;
+a.href=data.dest;
+a.target=data.new?"_blank":"";
+
+box=document.createElement("div");
+box.class="container";
+box.appendChild(a)
+document.body.appendChild(box);
+})
