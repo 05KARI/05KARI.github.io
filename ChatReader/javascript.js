307a978d2f815953c8cea8f025108b10ab92d537
05KARI <106485649+05KARI@users.noreply.github.com>
2023-11-09 12:28:27 +0200

    Add files via upload

---
+++ ChatReader/javascript.js
@@ -0,0 +1,20 @@
+function ReadFile(files){
+  fr=new FileReader();
+  fr.onload=function(){
+    try{CreateMessages(JSON.parse(fr.result));}
+    catch{alert("couldn't parse json string")}
+  }
+  fr.readAsText(files[0]);
+}
+
+function CreateMessages(data){
+  Messages=document.querySelector("div#Messages");
+  MessageList=document.querySelector("div#Holder");
+  Messages.innerHTML="";
+  data.forEach(e=>{
+    classes="class=\"Message Role"+e.Role+"\"";
+    console.log(e.Role+" says: "+e.Content)
+    Messages.innerHTML+=`<p title="${e.Role}" ${classes}>${e.Content}</p>`;
+  });
+  MessageList.style.display="block";
+}
